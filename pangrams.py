12be15d6c83dc37d2dae6d76b8268444cd9d3d5c
kelleydv <kelleydv@gmail.com>
2014-04-18 11:08:02 -0700

    initial commit: pangrams

---
+++ pangrams.py
@@ -0,0 +1,18 @@
+
+
+def ispangram():
+    """Function to check if the input string is a pangram
+
+    should accept a single value <string> and return True or False.
+
+        >>> x = "The quick brown fox jumps over the lazy dog."
+        >>> ispangram(x)
+        >>> True
+
+    Feel free to add any other funcitons to this module, but do
+    not change the name of ispangram().
+    """
+
+    # insert code here!
+
+    return
